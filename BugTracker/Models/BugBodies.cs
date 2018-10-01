d0fa10c8ffa6b775a57765c7abfdccbb7d84799f
Sergei <sbelialov@gmail.com>
2018-10-01 12:43:24 -0500

    Created base model data

---
+++ BugTracker/Models/BugBodies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BugTracker.Models
+{
+    public class BugBodies
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ BugTracker/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            Bugs = new List<Bug>();
+        }
+
+        public int Id { get; set; }
+
+        //foreign keys connections
+        public int AuthroId { get; set; }
+        public virtual ApplicationUser Author {get; set;}
+
+
+        // local properties
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Bug> Bugs { get; set; }
+
+
+
+    }
+}
