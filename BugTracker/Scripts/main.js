152e623cb0f7bcd3a05fc2b4605adb010e34a19a
Sergei <sbelialov@gmail.com>
2018-10-10 11:12:52 -0500

    DONE: JS script for seach input to search among tickets or projects.

---
+++ BugTracker/Scripts/main.js
@@ -0,0 +1,7 @@
+﻿document.querySelector("#srch-btn").addEventListener("click", (e) => {
+
+				let selecValue = document.querySelector("#lunch").value;
+				let searchQuery = document.querySelector("#search-bar").value;
+				window.location.href = `${selecValue}?query=${searchQuery}`;
+
+});
